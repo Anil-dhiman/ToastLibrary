9dd47b932aa03f0a95bf2e5c9b4f662658c8a6c6
anil dhiman <dhimananil2000@gmail.com>
2022-03-01 13:02:26 +0530

    vibrate

--- toastLibrary/src/main/java/com/toastlibrary/ToastMessage.kt
+++ toastLibrary/src/main/java/com/toastlibrary/ToastMessage.kt
@@ -1,13 +1,25 @@
 package com.toastlibrary
 
 import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+
 
 class ToastMessage {
 
     companion object {
         public fun showMessage(context:Context,message : String){
             Toast.makeText(context,"$message",Toast.LENGTH_SHORT).show()
+            val v = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v?.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                v?.vibrate(500)
+            }
         }
     }
 
